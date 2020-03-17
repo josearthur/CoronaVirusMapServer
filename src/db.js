45ca793d3ae96f33b8655b60031f3282d5b84528
JoseGerdes <josearthurgerdes@gmail.com>
2020-03-17 17:25:13 -0300

    Fix

--- src/db.js
+++ src/db.js
@@ -1,4 +1,11 @@
-const monk = require('monk');
-const db = monk('mongodb+srv://josegerdes:oiD0BWQ3BIVwlTh9@cluster0-con3n.mongodb.net/test?retryWrites=true&w=majority');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk(`mongodb+srv://josegerdes:oiD0BWQ3BIVwlTh9@cluster0-con3n.mongodb.net/test?retryWrites=true&w=majority`);
+
+db.then(() =>{
+  console.log("connection success");
+}).catch((e)=>{
+  console.error("Error !",e);
+});
 
 module.exports = db;
